fb64fe9085bbc25f5c88c1af992367ad4ef5f15e
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-08-30 19:03:11 +0100

    formatted code

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 let countriesData;
 
-
-console.log("Testing, testing, 1 2 3 ...")
-
 // fetch data from API
+
 const fetchCountries = async () => {
+    
     const response = await fetch("https://restcountries.com/v3.1/all");
     const jsonData = await response.json();
+    console.log(jsonData);
     return jsonData;
+    
 }
+
 // set up function
 
 const setUp = async () => {
-    countriesData = await fetchCountries();
-    fillCountriesList(countriesData);
+    countriesData = await fetchCountries(); 
+    fillCountriesList(countriesData);  // passes countries data into fillCountriesList function.
+ 
 }
 
-function fillCountriesList(jsonData) {
+// fills in list of countries
+
+function fillCountriesList(countries) {
     const countriesList = document.getElementById("countriesList");
     countriesList.innerHTML = "";
 
-    jsonData.forEach(country => {
+    countries.forEach(country => {
         const countryName = country.name.common;
         const countryPopulation = country.population;
 
         const countryLi = document.createElement('li');
         countryLi.textContent = `${countryName} - Population: ${countryPopulation}`;
         countriesList.appendChild(countryLi);
-
     });
 }
 
@@ -39,7 +43,9 @@ function formSubmit(event) {
     const searchText = searchInput.value.toLowerCase();
     filterCountries(searchText);
 }
+
 // filtered countries
+
 function filterCountries(searchText) {
     const filteredCountries = countriesData.filter(country => {
         const countryName = country.name.common.toLowerCase();
